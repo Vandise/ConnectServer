dd86373ed5fb345385a3fa9408a12a901b33721b
Benjamin J Anderson <banderson@mashable.com>
2016-05-21 18:17:53 -0500

    Clear interval after 999 status code sent

--- src/app/channels/ClientAuth.js
+++ src/app/channels/ClientAuth.js
@@ -5,6 +5,9 @@ const options = {
   timeout: 1000
 };
 
+let gsTimer = null;
+let pingCount = 0;
+
 export default (server, session) => {
   const socket = session.socket;
   socket.on('authenticate_client', (data) => {
@@ -12,14 +15,19 @@ export default (server, session) => {
     let authSocket = io.connect(`${data.host}:${data.port}`, options);
 
     // give 1.5s to see if the game server times out
-    setTimeout(() => {
-      if (!authSocket.connected) {
+    gsTimer = setInterval(() => {
+      if (authSocket.connected) {
+        clearInterval(gsTimer);
+      }
+      if (!authSocket.connected && pingCount === 5) {
         socket.emit('server_error', {
           message: '(999) Connection refused.',
           code: 999
         });
+        clearInterval(gsTimer);
       }
-    }, 1500);
+      pingCount++;
+    }, 250);
 
     authSocket.on('connect', () => {
 
