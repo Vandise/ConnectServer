e4d46763f0381b0581dd14efa7116836fdb34e60
Benjamin J Anderson <banderson@mashable.com>
2016-05-21 23:18:38 -0500

    Update client auth

--- src/app/channels/ClientAuth.js
+++ src/app/channels/ClientAuth.js
@@ -4,33 +4,39 @@ const options = {
   transports: ['websocket'],
   timeout: 1000
 };
-
-let gsTimer = null;
-let pingCount = 0;
-
+  
 export default (server, session) => {
   const socket = session.socket;
   socket.on('authenticate_client', (data) => {
 
-    let authSocket = io.connect(`${data.host}:${data.port}`, options);
+    let authSocket = io.connect(`http://${data.host}:${data.port}`, options);
+
+    let gsTimer = null;
+    let pingCount = 0;
+
+    server.logger.info(`Attempting to connect to GS: ${data.host}:${data.port}`);
 
     // give 1.5s to see if the game server times out
     gsTimer = setInterval(() => {
       if (authSocket.connected) {
         clearInterval(gsTimer);
+        return;
       }
-      if (!authSocket.connected && pingCount === 5) {
+      if (!authSocket.connected && pingCount % 5 === 0) {
+        server.logger.info(`Unable to connect to GS: ${data.host}:${data.port}`);
         socket.emit('server_error', {
           message: '(999) Connection refused.',
           code: 999
         });
+        authSocket.destroy();
         clearInterval(gsTimer);
+        return;
       }
       pingCount++;
     }, 250);
 
     authSocket.on('connect', () => {
-
+      server.logger.info(`Connected to GS: ${data.host}:${data.port}`);
       // emit the client version
       authSocket.emit('authenticate_client_version', {
         version: data.version
@@ -44,14 +50,14 @@ export default (server, session) => {
           socket.emit('authenticate_client', {
             status: 1
           });  
-          authSocket.disconnect();
+          authSocket.destroy();
 
         } else {
 
           socket.emit('authenticate_client', {
             status: 0
           });  
-          authSocket.disconnect();
+          authSocket.destroy();
 
         }
       });
